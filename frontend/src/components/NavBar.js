b5bf3dc0c29fceaca886b63f071cac0d34a857e0
Tim <tassmann@students.uni-mainz.de>
2022-10-25 00:39:57 +0200

    navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,28 +5,32 @@ import Typography from "@mui/material/Typography";
 import "../scss/nav-bar.scss";
 
 const NavBar = () => {
-  return (
-    <div className="nav-bar-container">
-      <AppBar position="static" sx={{ height: "74px" }}>
-        <Toolbar variant="dense">
-          <Typography
-            sx={{
-              height: "100%",
-              width: "100%",
-              fontSize: "24px",
-              textAlign: "center",
-              paddingTop: "32px"
-            }}
-            variant="h6"
-            color="inherit"
-            component="div"
-          >
-            Terminplaner
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+   return (
+      <div className="nav-bar-container">
+         <AppBar
+            position="static"
+            sx={{ height: "74px", backgroundColor: "rgba(24, 118, 210, .6)" }}
+         >
+            <Toolbar variant="dense">
+               <Typography
+                  sx={{
+                     height: "100%",
+                     width: "100%",
+                     fontSize: "28px",
+                     textAlign: "center",
+                     paddingTop: "32px",
+                     fontWeight: "500",
+                  }}
+                  variant="h6"
+                  color="white"
+                  component="div"
+               >
+                  Terminplaner
+               </Typography>
+            </Toolbar>
+         </AppBar>
+      </div>
+   );
 };
 
 export default NavBar;
