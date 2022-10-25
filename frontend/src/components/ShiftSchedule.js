d7e507deda2454f9df9e208b049b372f275a4560
Tim <t.assmann00@web.de>
2022-10-25 15:48:14 +0200

    drag and drop

--- frontend/src/components/ShiftSchedule.js
+++ frontend/src/components/ShiftSchedule.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
@@ -8,205 +8,186 @@ import ShiftScheduleTopic from "./ShiftScheduleTopic";
 import ShiftScheduleTimeslot from "./ShiftScheduleTimeslot";
 import ShiftScheduleEmployee from "./ShiftScheduleEmployee";
 
-const tempData = [
-   {
-      slotTime: "08:00 - 10:00",
-      "08:00 - 10:00": [{ firstname: "Max", lastname: "Mustermann" }],
-   },
-   {
-      slotTime: "10:00 - 12:00",
-      "10:00 - 12:00": [{ firstname: "Max", lastname: "Mustermann" }],
-   },
-   {
-      slotTime: "12:00 - 14:00",
-      "12:00 - 14:00": [{ firstname: "Max", lastname: "Mustermann" }],
-   },
-   {
-      slotTime: "16:00 - 18:00",
-      "16:00 - 18:00": [{ firstname: "Max", lastname: "Mustermann" }],
-   },
-   /*
-  { slotTime: "08:00 - 09:00" },
-  { slotTime: "09:00 - 10:00" },
-  { slotTime: "10:00 - 11:00" },
-  { slotTime: "11:00 - 12:00" },
-  { slotTime: "12:00 - 13:00" },
-  { slotTime: "13:00 - 14:00" },
-  { slotTime: "14:00 - 15:00" },
-  { slotTime: "15:00 - 16:00" },
-  { slotTime: "16:00 - 17:00" },*/
+const tempDay = [
+  "08:00 - 10:00",
+  "10:00 - 12:00",
+  "12:00 - 14:00",
+  "14:00 - 16:00",
+  "16:00 - 18:00",
+];
+
+const tempShift = [
+  {
+    "08:00 - 10:00": [3],
+    "10:00 - 12:00": [3],
+    "12:00 - 14:00": [],
+    "14:00 - 16:00": [4],
+    "16:00 - 18:00": [4],
+  },
+  {
+    "08:00 - 10:00": [2],
+    "10:00 - 12:00": [2],
+    "12:00 - 14:00": [],
+    "14:00 - 16:00": [4],
+    "16:00 - 18:00": [1],
+  },
+  {
+    "08:00 - 10:00": [1],
+    "10:00 - 12:00": [1],
+    "12:00 - 14:00": [],
+    "14:00 - 16:00": [1],
+    "16:00 - 18:00": [1],
+  },
+  {
+    "08:00 - 10:00": [4],
+    "10:00 - 12:00": [4],
+    "12:00 - 14:00": [],
+    "14:00 - 16:00": [4],
+    "16:00 - 18:00": [2],
+  },
+  {
+    "08:00 - 10:00": [3],
+    "10:00 - 12:00": [3, 4],
+    "12:00 - 14:00": [4],
+    "14:00 - 16:00": [4],
+    "16:00 - 18:00": [1],
+  },
 ];
 
 const tempEmployee = [
-   {
-      key: 1,
-      firstname: "Max",
-      lastname: "Mustermann",
-      duration: 8,
-   },
-   {
-      key: 2,
-      firstname: "Marie",
-      lastname: "Musterfrau",
-      duration: 8,
-   },
-   {
-      key: 3,
-      firstname: "Florian",
-      lastname: "Schroeter",
-      duration: 6,
-   },
-   {
-      key: 4,
-      firstname: "Tim",
-      lastname: "Assmann",
-      duration: 4,
-   },
-   /*
-   {
-      key: 1,
-      firstname: "Max",
-      lastname: "Mustermann",
-      duration: 8,
-   },
-   {
-      key: 2,
-      firstname: "Marie",
-      lastname: "Musterfrau",
-      duration: 8,
-   },
-   {
-      key: 1,
-      firstname: "Max",
-      lastname: "Mustermann",
-      duration: 8,
-   },
-   {
-      key: 2,
-      firstname: "Marie",
-      lastname: "Musterfrau",
-      duration: 8,
-   },
-   {
-      key: 1,
-      firstname: "Max",
-      lastname: "Mustermann",
-      duration: 8,
-   },
-   {
-      key: 2,
-      firstname: "Marie",
-      lastname: "Musterfrau",
-      duration: 8,
-   },
-   {
-      key: 1,
-      firstname: "Max",
-      lastname: "Mustermann",
-      duration: 8,
-   },
-   {
-      key: 2,
-      firstname: "Marie",
-      lastname: "Musterfrau",
-      duration: 8,
-   },*/
+  {
+    key: 1,
+    firstname: "Max",
+    lastname: "Mustermann",
+    duration: 8,
+  },
+  {
+    key: 2,
+    firstname: "Marie",
+    lastname: "Musterfrau",
+    duration: 8,
+  },
+  {
+    key: 3,
+    firstname: "Florian",
+    lastname: "Schroeter",
+    duration: 6,
+  },
+  {
+    key: 4,
+    firstname: "Tim",
+    lastname: "Assmann",
+    duration: 4,
+  },
 ];
 
 const ShiftSchedule = () => {
-   const [employees, setEmployees] = useState(tempEmployee);
+  const [employees, setEmployees] = useState(tempEmployee);
+  const [shift, setShift] = useState(tempShift);
+  const [change, setChange] = useState(0);
 
-   const handleOnDragEnd = (result) => {
-      console.log(result);
-      const items = Array.from(employees);
-      const [recordedItems] = items.splice(result.source.index, 1);
-      items.splice(result.destination.index, 0, recordedItems);
+  useEffect(() => {}, [shift]);
 
-      setEmployees(items);
-   };
+  const handleOnDragEnd = (result) => {
+    const empId = result.source.index + 1;
+    const dropId = result.destination.droppableId;
+    const day = dropId.split("-")[3];
+    const timeSlot = dropId.split("-")[2];
+    var temp = shift;
+    //console.log(temp[day]["08:00 - 10:00"]);
+    temp[day][tempDay[timeSlot]].push(empId);
+    //console.log(temp[day][tempDay[timeSlot]]);
+    setShift(temp);
+    setChange(change + 1);
+    /*
+    const items = Array.from(employees);
+    const [recordedItems] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, recordedItems);
 
-   return (
-      <>
-         <div>
-            <Box
-               sx={{
-                  width: "85vw",
-                  height: "75vh",
-                  boxShadow: 3,
-                  borderRadius: 2,
-               }}
-            >
-               <ShiftScheduleTopic></ShiftScheduleTopic>
-               <div className="shift-content">
-                  <div className="shift-content-timetable">
-                     {tempData.map((slot, index) => {
-                        const height = 94 / tempData.length - 0.6;
-                        const lastElement = index === tempData.length - 1;
-                        return (
-                           <div key={index} style={{ height: `${height}%` }}>
-                              <ShiftScheduleTimeslot
-                                 data={slot}
-                                 height={100}
-                                 index={index}
-                              ></ShiftScheduleTimeslot>
-                              {lastElement ? (
-                                 <></>
-                              ) : (
-                                 <Divider sx={{ width: "100%" }} />
-                              )}
-                           </div>
-                        );
-                     })}
-                  </div>
+    setEmployees(items);*/
+    console.log(shift);
+  };
 
-                  <DragDropContext onDragEnd={handleOnDragEnd}>
-                     <Droppable droppableId="character">
-                        {(provided) => (
-                           <div
-                              className="character shift-content-employee"
-                              {...provided.droppableProps}
+  return (
+    <>
+      <div>
+        <Box
+          sx={{
+            width: "85vw",
+            height: "75vh",
+            boxShadow: 3,
+            borderRadius: 2,
+          }}
+        >
+          <ShiftScheduleTopic></ShiftScheduleTopic>
+          <div className="shift-content">
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+              <div className="shift-content-timetable">
+                {tempDay.map((slot, index) => {
+                  const height = 94 / tempDay.length - 0.6;
+                  const lastElement = index === tempDay.length - 1;
+                  return (
+                    <div
+                      key={`${index}-${change}`}
+                      style={{ height: `${height}%` }}
+                    >
+                      <ShiftScheduleTimeslot
+                        shift={shift}
+                        employees={tempEmployee}
+                        time={slot}
+                        height={100}
+                        index={index}
+                      ></ShiftScheduleTimeslot>
+                      {lastElement ? <></> : <Divider sx={{ width: "100%" }} />}
+                    </div>
+                  );
+                })}
+              </div>
+
+              <Droppable droppableId="character">
+                {(provided) => (
+                  <div
+                    className="character shift-content-employee"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {employees.map((emp, index) => {
+                      const lastElement = index === employees.length - 1;
+                      return (
+                        <Draggable
+                          key={index}
+                          draggableId={`drag-emp-${index}`}
+                          index={index}
+                        >
+                          {(provided) => (
+                            <div
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
                               ref={provided.innerRef}
-                           >
-                              {employees.map((emp, index) => {
-                                 const lastElement =
-                                    index === tempData.length - 1;
-                                 return (
-                                    <Draggable
-                                       key={index}
-                                       draggableId={`drag-emp-${index}`}
-                                       index={index}
-                                    >
-                                       {(provided) => (
-                                          <div
-                                             {...provided.draggableProps}
-                                             {...provided.dragHandleProps}
-                                             ref={provided.innerRef}
-                                          >
-                                             <ShiftScheduleEmployee
-                                                employee={emp}
-                                             ></ShiftScheduleEmployee>
-                                             {lastElement ? (
-                                                <></>
-                                             ) : (
-                                                <Divider
-                                                   sx={{ width: "100%" }}
-                                                />
-                                             )}
-                                          </div>
-                                       )}
-                                    </Draggable>
-                                 );
-                              })}
-                              {provided.placeholder}
-                           </div>
-                        )}
-                     </Droppable>
-                  </DragDropContext>
-               </div>
-            </Box>
-         </div>
-      </>
-   );
+                            >
+                              <ShiftScheduleEmployee
+                                employee={emp}
+                              ></ShiftScheduleEmployee>
+                              {lastElement ? (
+                                <></>
+                              ) : (
+                                <Divider sx={{ width: "100%" }} />
+                              )}
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
+        </Box>
+      </div>
+    </>
+  );
 };
 
 export default ShiftSchedule;
--- frontend/src/components/ShiftScheduleTimeslot.js
+++ frontend/src/components/ShiftScheduleTimeslot.js
@@ -1,61 +1,86 @@
 import React from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 import "../scss/shift-schedule.scss";
 
-const ShiftScheduleTimeslot = ({ data, height, index }) => {
-   return (
-      <>
-         <Box
-            sx={{
-               width: "100%",
-               height: `${height}%`,
-               marginTop: "0.6%",
-               display: "flex",
-               alignItems: "center",
-               padding: 0,
-            }}
-         >
-            <Typography
-               sx={{
-                  width: "14%",
-                  height: "70%",
-                  borderRight: "1px solid lightgray",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-               }}
-               variant="h6"
-               color="inherit"
-               component="div"
+const NameMap = ({ names }) => {
+  return (
+    <div>
+      {names.map((name, index) => {
+        return (
+          <label key={index}>
+            {name}
+            <br></br>
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+
+const ShiftScheduleTimeslot = ({ shift, time, employees, height, index }) => {
+  console.log(shift);
+  return (
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          height: `${height}%`,
+          marginTop: "0.6%",
+          display: "flex",
+          alignItems: "center",
+          padding: 0,
+        }}
+      >
+        <Typography
+          sx={{
+            width: "14%",
+            height: "70%",
+            borderRight: "1px solid lightgray",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          variant="h6"
+          color="inherit"
+          component="div"
+        >
+          {time}
+        </Typography>
+
+        {shift.map((day, shiftIdx) => {
+          var names = [];
+          employees.forEach((emp) => {
+            day[time].map((empSlot) => {
+              if (emp.key === empSlot) names.push(emp.firstname);
+            });
+          });
+
+          return (
+            <Droppable
+              key={`${index}-${shiftIdx}`}
+              droppableId={`time-slot-${index}-${shiftIdx}`}
             >
-               {data.slotTime}
-            </Typography>
-            <DragDropContext>
-               {[1, 2, 3, 4, 5].map((number) => {
-                  return (
-                     <Droppable
-                        key={number}
-                        droppableId={`time-slot-${index}-${number}`}
-                     >
-                        {(provided) => (
-                           <div
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                              ref={provided.innerRef}
-                              id={`emp-${number}-${index}`}
-                              className="employee-in-timeslot"
-                           ></div>
-                        )}
-                     </Droppable>
-                  );
-               })}
-            </DragDropContext>
-         </Box>
-      </>
-   );
+              {(provided) => (
+                <div
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  ref={provided.innerRef}
+                  id={`emp-${shiftIdx}-${index}`}
+                  className="employee-in-timeslot"
+                >
+                  {provided.placeholder}
+                  <NameMap names={names}></NameMap>
+                </div>
+              )}
+            </Droppable>
+          );
+        })}
+      </Box>
+    </>
+  );
 };
 
 export default ShiftScheduleTimeslot;
