ea2b24e2cbcc4a2462df6dc5b80c597c0dd73b20
Flo <fschroet@students.uni-mainz.de>
2022-10-25 12:19:16 +0200

    Drag and drop changes

---
+++ backend/api/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import *
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ["id", "firstname", "lastname", "duration", "preferences"]
